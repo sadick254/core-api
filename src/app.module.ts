2e48be1c9454dc7f28044b342a7f9acc715527c5
sadick254 <sadickjunior@gmail.com>
2023-02-26 21:11:49 +0300

    Add a configuration mechanism.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { CustomerModule } from './customers/customer.module';
 import { IdentityModule } from './identity/identity.module';
 import { LoginModule } from './login/login.module';
@@ -7,9 +8,10 @@ import { UserModule } from './users/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'okra.sqlite',
+      database: process.env.DB_NAME,
       synchronize: true,
       autoLoadEntities: true,
     }),
